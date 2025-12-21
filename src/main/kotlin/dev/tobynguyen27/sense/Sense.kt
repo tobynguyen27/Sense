61c7d4e10c2da2a7591a68058c2388bff0fa9732
Toby Nguyen <208221158+tobynguyen27@users.noreply.github.com>
2025-12-22 00:16:34 +0700

    format

--- src/main/kotlin/dev/tobynguyen27/sense/Sense.kt
+++ src/main/kotlin/dev/tobynguyen27/sense/Sense.kt
@@ -3,13 +3,12 @@ package dev.tobynguyen27.sense
 import net.fabricmc.api.ModInitializer
 import org.slf4j.LoggerFactory
 
-object Sense: ModInitializer {
+object Sense : ModInitializer {
 
     const val MOD_ID = "sense"
     const val MOD_NAME = "Sense"
 
     val LOGGER = LoggerFactory.getLogger(MOD_NAME)
 
-    override fun onInitialize() {
-    }
+    override fun onInitialize() {}
 }
