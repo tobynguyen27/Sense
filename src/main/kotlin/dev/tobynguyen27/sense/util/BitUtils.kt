7c93b14f1e4fe44911ce03d2dbd24062ce6016a4
Toby Nguyen <208221158+tobynguyen27@users.noreply.github.com>
2025-12-22 21:34:01 +0700

    add BitUtils

---
+++ src/main/kotlin/dev/tobynguyen27/sense/util/BitUtils.kt
@@ -0,0 +1,29 @@
+package dev.tobynguyen27.sense.util
+
+object BitUtils {
+
+    /**
+     * Get the [index]-th bit in [number]
+     *
+     * For example: Get bit at [index] = 3: `101010` -> 1
+     *
+     * **NOTE:** Returns 0 if the [index] is out of range
+     */
+    fun getBitAt(index: Int, number: Long): Long = (number shr index) and 1L
+
+    /**
+     * Turn off the index-th in [number].
+     */
+    fun turnOffBitAt(index: Int, number: Long): Long = number and (1L shl index).inv()
+
+    /**
+     * Turn on the index-th in [number].
+     */
+    fun turnOnBitAt(index: Int, number: Long): Long = number or (1L shl index)
+
+    /**
+     * Fip the index-th in [number].
+     */
+    fun flipBitAt(index: Int, number: Long): Long = number xor (1L shl index)
+
+}
---
+++ src/test/kotlin/dev/tobynguyen27/sense/util/BitUtilsTest.kt
@@ -0,0 +1,42 @@
+package dev.tobynguyen27.sense.util
+
+import dev.tobynguyen27.sense.util.BitUtils.flipBitAt
+import dev.tobynguyen27.sense.util.BitUtils.getBitAt
+import dev.tobynguyen27.sense.util.BitUtils.turnOffBitAt
+import dev.tobynguyen27.sense.util.BitUtils.turnOnBitAt
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class BitUtilsTest {
+
+    val bits = 50L // 110011
+
+    @Test
+    fun `test getBitAt`() {
+        val expected = 1L
+
+        assertEquals(expected, getBitAt(4, bits))
+    }
+
+    @Test
+    fun `test turnOnBitAt`() {
+        val expected = 58L
+
+        assertEquals(expected, turnOnBitAt(3, bits))
+    }
+
+    @Test
+    fun `test turnOffBitAt`() {
+        val expected = 50L
+
+        assertEquals(expected, turnOffBitAt(3, bits))
+    }
+
+    @Test
+    fun `test flipBitAt`() {
+        val expected = 51L
+
+        assertEquals(expected, flipBitAt(0, bits))
+    }
+
+}
