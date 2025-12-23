896668bdfb23057ba5ebbe9d43d533db343334c7
Toby Nguyen <208221158+tobynguyen27@users.noreply.github.com>
2025-12-23 11:20:58 +0700

    add Permanent annotation

---
+++ src/main/kotlin/dev/tobynguyen27/sense/sync/annotation/Permanent.kt
@@ -0,0 +1,8 @@
+package dev.tobynguyen27.sense.sync.annotation
+
+import dev.tobynguyen27.sense.sync.SenseBlockEntity
+
+/**
+ * Apply this to a field in your [SenseBlockEntity] will make it saved and loaded NBT data automatically.
+ */
+@Target(AnnotationTarget.FIELD) annotation class Permanent(val key: String = "")
