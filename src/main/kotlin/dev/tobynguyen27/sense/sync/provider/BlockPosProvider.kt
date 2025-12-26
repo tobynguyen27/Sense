6a6b157959582b7065edac04f8f178dd6f3d6d65
Toby Nguyen <208221158+tobynguyen27@users.noreply.github.com>
2025-12-26 08:37:41 +0700

    format

--- src/main/kotlin/dev/tobynguyen27/sense/sync/provider/BlockPosProvider.kt
+++ src/main/kotlin/dev/tobynguyen27/sense/sync/provider/BlockPosProvider.kt
@@ -2,13 +2,13 @@ package dev.tobynguyen27.sense.sync.provider
 
 import dev.tobynguyen27.sense.sync.accessor.Accessor
 import dev.tobynguyen27.sense.sync.accessor.BlockPosAccessor
+import java.lang.invoke.MethodHandles
+import java.lang.reflect.Field
 import net.minecraft.core.BlockPos
 import net.minecraft.nbt.CompoundTag
 import net.minecraft.nbt.NbtUtils
-import java.lang.invoke.MethodHandles
-import java.lang.reflect.Field
 
-object BlockPosProvider: AccessorProvider {
+object BlockPosProvider : AccessorProvider {
 
     private val LOOKUP = MethodHandles.lookup()
 
@@ -16,18 +16,16 @@ object BlockPosProvider: AccessorProvider {
         return field.type == BlockPos::class.java
     }
 
-    override fun create(
-        name: String,
-        field: Field,
-        owner: Any
-    ): Accessor {
+    override fun create(name: String, field: Field, owner: Any): Accessor {
         val getter = LOOKUP.unreflectGetter(field).bindTo(owner)
         val setter = LOOKUP.unreflectSetter(field).bindTo(owner)
 
-        return BlockPosAccessor(name,
+        return BlockPosAccessor(
+            name,
             { getter.invoke() as BlockPos },
-            {setter.invoke(it)},
-            {tag, name -> NbtUtils.readBlockPos(tag.get(name) as CompoundTag)},
-            {tag, name, pos -> tag.put(name, NbtUtils.writeBlockPos(pos))})
+            { setter.invoke(it) },
+            { tag, name -> NbtUtils.readBlockPos(tag.get(name) as CompoundTag) },
+            { tag, name, pos -> tag.put(name, NbtUtils.writeBlockPos(pos)) },
+        )
     }
 }
