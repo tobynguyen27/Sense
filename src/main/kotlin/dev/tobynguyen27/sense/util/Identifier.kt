2bf6df208e0216e29a1dfd68093615183a369763
Toby Nguyen <208221158+tobynguyen27@users.noreply.github.com>
2025-12-31 10:48:57 +0700

    make Identifier be an internal class

--- src/main/kotlin/dev/tobynguyen27/sense/util/Identifier.kt
+++ src/main/kotlin/dev/tobynguyen27/sense/util/Identifier.kt
@@ -3,4 +3,4 @@ package dev.tobynguyen27.sense.util
 import dev.tobynguyen27.sense.Sense
 import net.minecraft.resources.ResourceLocation
 
-class Identifier(path: String) : ResourceLocation(Sense.MOD_ID, path) {}
+internal class Identifier(path: String) : ResourceLocation(Sense.MOD_ID, path) {}
