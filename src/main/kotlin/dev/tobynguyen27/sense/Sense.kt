8fb6e944be64c9d9521f564b92baed44507a224a
Toby Nguyen <208221158+tobynguyen27@users.noreply.github.com>
2025-12-31 11:33:07 +0700

    add initializing text

--- src/main/kotlin/dev/tobynguyen27/sense/Sense.kt
+++ src/main/kotlin/dev/tobynguyen27/sense/Sense.kt
@@ -1,6 +1,7 @@
 package dev.tobynguyen27.sense
 
 import net.fabricmc.api.ModInitializer
+import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 
 object Sense : ModInitializer {
@@ -8,7 +9,9 @@ object Sense : ModInitializer {
     const val MOD_ID = "sense"
     const val MOD_NAME = "Sense"
 
-    val LOGGER = LoggerFactory.getLogger(MOD_NAME)
+    val LOGGER: Logger = LoggerFactory.getLogger(MOD_NAME)
 
-    override fun onInitialize() {}
+    override fun onInitialize() {
+        LOGGER.info("Sense is waking up")
+    }
 }
