a69fdc8a5355780886673280fb83cfdede516833
Toby Nguyen <208221158+tobynguyen27@users.noreply.github.com>
2025-12-31 11:16:50 +0700

    make Packets internal as well

--- src/main/kotlin/dev/tobynguyen27/sense/network/Packets.kt
+++ src/main/kotlin/dev/tobynguyen27/sense/network/Packets.kt
@@ -2,7 +2,7 @@ package dev.tobynguyen27.sense.network
 
 import dev.tobynguyen27.sense.util.Identifier
 
-object Packets {
+internal object Packets {
 
     val SYNC_DATA_PACKET = Identifier("sync_data")
 }
