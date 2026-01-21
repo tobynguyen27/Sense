606120527aad974cdbb8a3a70d6dbecfada226a8
Toby Nguyen <208221158+tobynguyen27@users.noreply.github.com>
2026-01-22 00:59:01 +0700

    Add some utils function for primitive types

---
+++ src/main/kotlin/dev/tobynguyen27/sense/util/PrimitiveUtils.kt
@@ -0,0 +1,15 @@
+package dev.tobynguyen27.sense.util
+
+object PrimitiveUtils {
+
+    /**
+     * Returns `0` if **true** else `1`
+     */
+    fun Boolean.toInt() = if (this) 0 else 1
+
+    /**
+     * Returns `true` if **0** else `false`
+     */
+    fun Int.toBoolean() = this == 0
+
+}
